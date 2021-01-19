886452c2ce432b5c5a950637c02d3451d70cfbd9
Luke Gill <bagrad007@gmail.com>
2021-01-19 15:57:42 -0500

    commit tree 6f75c42ff5bd3c0bc20e9472bccf486125ae42ba

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+     def initialize(word_to_scramble)
+        @word_to_scramble = word_to_scramble
+    end
+
+    def match(words)
+        words.select do |word|
+            word.chars.sort == @word_to_scramble.chars.sort
+        end
+    end
+end
